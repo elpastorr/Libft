f073927cc7e4ac4bbe93c4012fcdcea3a91bbce6
elpastorr <89456448+elpastorr@users.noreply.github.com>
2021-12-03 14:44:23 +0100

    Add files via upload

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: elpastor <elpastor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 14:14:03 by elpastor          #+#    #+#             */
-/*   Updated: 2021/11/29 17:23:46 by elpastor         ###   ########.fr       */
+/*   Updated: 2021/12/03 14:41:13 by elpastor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,5 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (i < n - 1 && s1[i] == s2[i] && s1[i])
 		i++;
-	return (s1[i] - s2[i]);
+	return ((const unsigned char)s1[i] - (const unsigned char)s2[i]);
 }
